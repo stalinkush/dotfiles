50e6ef197a9f909959366f0a1c3cca4803801165
StalinKush <stalinsampras@live.com>
2025-11-27 21:39:17 +0530

    Added sudo command to mv

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "Trying to install SCC"
 # Download and extract scc
 wget -qO scc.tar.gz https://github.com/boyter/scc/releases/latest/download/scc_Linux_x86_64.tar.gz
 tar xf scc.tar.gz
-mv scc ~../../bin/
+sudo mv scc ~../../bin/
 rm scc.tar.gz
 
 # Ensure ~/bin is in PATH
